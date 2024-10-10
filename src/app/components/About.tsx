a3e267bd904c3583587f0435f55c7f12da4def92
Eliyahu-Yishai <eli089743@gmail.com>
2024-10-10 18:08:42 +0300

    Refactor About component to update content and add expertise details

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -17,13 +17,22 @@ const About:React.FC = () => {
             <h1 className={styles.mainName}>Eliyahu yishai</h1>
             <h1 className={styles.secondTitle}>Software engineering</h1>
             <p className={styles.contentMain}>
-                Hi, I&apos;m Eliyahu | Yishai, a software engineering 
-                 i have a passion for creating and developing software applications.
-                 Specializing in web development, mobile apps. 
+                Hi, I&apos;m Eliyahu Yishai, a software engineer,
+                I have a passion for creating and developing software applications.
+                Specializing in <strong><em>web development</em></strong>, <strong><em>mobile apps</em></strong>, etc.
+                <br/>
+                <br/>
+                My biggest enjoyment in the world of software is to watch how the development happens
+                and grows before your eyes, gaining value, volume and seeing the result
+                of what I do appear.
+                <br/>
+                <br/>
+                My expertise now spans main languages such as <strong><em>JavaScript</em></strong>, <strong><em>TypeScript</em></strong>
+                and also frameworks: <strong><em>React</em></strong>, <strong><em>ReactNative</em></strong>, <strong><em>NextJs</em></strong>.
             </p>
         </div>
       </div>
-    )
+    );
 }
 
 export default About;
